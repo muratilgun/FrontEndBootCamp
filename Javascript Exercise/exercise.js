016b966f7a9109303afcbbad5416c6fb6095d8bc
muratilgun <muratilgun34@gmail.com>
2022-11-08 22:48:21 +0300

    Javascript-9

--- Javascript Exercise/exercise.js
+++ Javascript Exercise/exercise.js
@@ -95,3 +95,12 @@ function getMilk(money){
 }
 
 getMilk(2);
+
+
+function lifeInWeeks(age){
+  var yearsRemaining = 90 - age;
+  var days = yearsRemaining * 365;
+  var weeks = yearsRemaining * 52;
+  var months = yearsRemaining *  12;
+  console.log("You have"+days+" days,"+weeks +" weeks, and"+months+" months left.");
+}
