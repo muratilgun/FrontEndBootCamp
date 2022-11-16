6183e2e99d2aa26134e0ed403caabbeffa257eae
muratilgun <muratilgun34@gmail.com>
2022-11-16 22:27:07 +0300

    Javascript-18

--- Javascript Exercise/exercise.js
+++ Javascript Exercise/exercise.js
@@ -312,3 +312,23 @@ while (numberOfBottles >= 0) {
     console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
 }
 }
+
+
+function fibonacciGenerator(n){
+var output = [];
+if(n === 1)
+{
+  output[0];
+}
+else if (n === 2) {
+  output[0,1];
+}
+else {
+  output[0,1];
+  for (var i = 2; i < n; i++) {
+    output.push(output[output.length-2] + output[output.length-1]);
+  }
+
+}
+return output;
+}
