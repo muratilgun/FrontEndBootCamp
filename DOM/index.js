4c731cec6ef349b626ba56247418ac4f6d57073c
muratilgun <muratilgun34@gmail.com>
2022-11-17 21:25:19 +0300

    Javascript-22

--- DOM/index.js
+++ DOM/index.js
@@ -8,3 +8,12 @@
 // document.getElementsByTagName("li");
 // document.getElementsByTagName("li")[2].style.color = "purple";
 // document.getElementById("title").innerHTML = "Hi";
+
+// querySelector
+// document.querySelector("h1");
+// document.querySelector("#title");
+// document.querySelector(".btn");
+// document.querySelector("li a");
+// document.querySelector("li.item");
+// querySelectorAll
+// document.querySelectorAll("#list .item")[2].style.color = "blue";
