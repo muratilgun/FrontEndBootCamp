1b177d1c23cf1288e0dbecd38956137d8c769a1c
muratilgun <muratilgun34@gmail.com>
2022-11-27 23:42:30 +0300

    Advanced Javascript-4

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -7,3 +7,16 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
   });
 
 }
+
+
+function add(num1, num2){
+  return num1+num2;
+}
+
+function multiply(num1, num2){
+  return num1+num2;
+}
+
+function calculator(num1,num2,operator){
+  return operator(num1,num2);
+}
