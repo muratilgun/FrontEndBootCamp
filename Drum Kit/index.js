c9052d412b0e098fe2c3d4ab499aca892ae71410
muratilgun <muratilgun34@gmail.com>
2022-11-28 22:55:07 +0300

    Advanced Javascript-5

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -2,21 +2,51 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click",function() {
-    alert("I got clicked");
-    //What to do when click detected.
+console.log(this);
+
+
+
   });
 
 }
 
 
-function add(num1, num2){
-  return num1+num2;
-}
 
-function multiply(num1, num2){
-  return num1+num2;
-}
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
 
-function calculator(num1,num2,operator){
-  return operator(num1,num2);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function add(num1, num2) {
+// return num1 + num2;
+// }
+//
+// function subtract(num1, num2) {
+// return num1 - num2;
+// }
+//
+// function multiply(num1, num2) {
+// return num1 * num2;
+// }
+//
+// function divide(num1, num2) {
+// return num1 / num2;
+// }
+//
+// function calculator(num1, num2, operator) {
+// return operator(num1, num2);
+// }
