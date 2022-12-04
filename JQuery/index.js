04d173aa8945b610bd24d7d4a7a313c8bef29fae
muratilgun <muratilgun34@gmail.com>
2022-12-04 23:02:34 +0300

    JQuery-4

--- JQuery/index.js
+++ JQuery/index.js
@@ -2,5 +2,29 @@ $(document).ready(function(){
   // $("h1").addClass("big-title margin-50");
 // $("h1").hasClass("margin-50")
 // $("button").html("<em>Hey</em>")
-$("a").attr("href","https://yahoo.com")
+// $("a").attr("href","https://yahoo.com")
+// $("h1").css("color","purple");
+
+
+// Javascript
+// for (var i = 0; i < 5; i++) {
+//   document.querySelectorAll("button")[i].addEventListener("click",function(){
+//     document.querySelector("h1").style.color = "purple";
+//   })
+// }
+
+//Jquery
+// $("button").click(function(){
+//   $("h1").css("color","purple");
+// })
+
+// $(document).keypress(function(event){
+//     $("h1").text(event.key);
+// });
+
+$("h1").on("mouseover",function(){
+  $("h1").css("color","purple");
+})
+
+
 })
