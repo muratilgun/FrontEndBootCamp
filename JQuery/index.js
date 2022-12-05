70e448f6c36cb732b4b44dc82e930b201f7aa6d5
muratilgun <muratilgun34@gmail.com>
2022-12-05 21:39:17 +0300

    JQuery-6

--- JQuery/index.js
+++ JQuery/index.js
@@ -40,4 +40,21 @@ $(document).ready(function(){
 // <h1>Hello.<a>new</a></h1>
 
 // $("a").remove();
+
+
+
+$("button").click(function(){
+  // $("h1").hide();
+  // $("h1").show();
+  // $("h1").toggle();
+  // $("h1").fadeOut();
+  // $("h1").fadeIn();
+  // $("h1").fadeToggle();
+  // $("h1").slideUp();
+  // $("h1").slideDown();
+  // $("h1").slideToggle();
+  // $("h1").animate({opacity: 0.5});
+  $("h1").slideUp().slideDown().animate({opacity: 0.5});
+
+})
 })
