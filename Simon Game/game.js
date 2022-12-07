df37d464f9a17f258427c0584d7587dfc11adea4
muratilgun <muratilgun34@gmail.com>
2022-12-07 23:12:09 +0300

    Simon Game-4

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -1,16 +1,18 @@
-var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
 
-function nextSequence(){
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
+$(".btn").click(function() {
+var userChosenColour = $(this).attr("id");
+userClickedPattern.push(userChosenColour);
+});
 
+function nextSequence() {
+var randomNumber = Math.floor(Math.random() * 4);
+var randomChosenColour = buttonColours[randomNumber];
+gamePattern.push(randomChosenColour);
+$("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-  var randomChosenColour =buttonColours[nextSequence()];
-    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
-
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
+var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+audio.play();
 }
