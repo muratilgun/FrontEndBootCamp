8968602bc917dec07b0b47da898eedd93fd43393
muratilgun <muratilgun34@gmail.com>
2023-01-01 21:45:33 +0300

    Simon Game-9

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -23,30 +23,34 @@ $(".btn").click(function() {
   playSound(userChosenColour);
   animatePress(userChosenColour);
 
-
   checkAnswer(userClickedPattern.length-1);
 });
 
 
 function checkAnswer(currentLevel) {
 
-
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
 
       console.log("success");
 
       if (userClickedPattern.length === gamePattern.length){
-
         setTimeout(function () {
           nextSequence();
         }, 1000);
-
       }
 
     } else {
 
       console.log("wrong");
 
+      playSound("wrong");
+
+      $("body").addClass("game-over");
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      $("#level-title").text("Game Over, Press Any Key to Restart");
     }
 
 }
@@ -54,7 +58,6 @@ function checkAnswer(currentLevel) {
 function nextSequence() {
 
   userClickedPattern = [];
-
   level++;
   $("#level-title").text("Level " + level);
 
